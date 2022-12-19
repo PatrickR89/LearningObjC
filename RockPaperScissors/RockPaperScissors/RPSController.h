9f0b4e4abb81f4becee8ff5d949d12d0bfa48081
PatrickR <patrick@ruzman.net>
2022-12-19 11:17:01 +0100

    ObjC RSPGame - [Add] Controller class
    - [Add] methods to Controller
    - [Add] define throDown method

---
+++ RockPaperScissors/RockPaperScissors/RPSController.h
@@ -0,0 +1,24 @@
+//
+//  RSPController.h
+//  RockPaperScissors
+//
+//  Created by Patrick on 19.12.2022..
+//
+
+#import <Foundation/Foundation.h>
+#import "RPSMove.h"
+#import "RPSGame.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSPController : NSObject
+
+@property (nonatomic) RPSGame *game;
+
+- (void)throwDown: (Move) playersTurn;
+- (Move)generateMove;
+- (BOOL)defeats: (RPSMove*) move;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RockPaperScissors/RockPaperScissors/RPSController.m
@@ -0,0 +1,26 @@
+//
+//  RSPController.m
+//  RockPaperScissors
+//
+//  Created by Patrick on 19.12.2022..
+//
+
+#import "RPSController.h"
+
+@implementation RSPController
+
+- (void)throwDown:(Move)playersTurn {
+    RPSMove *playerTurn = [[RPSMove alloc] initWithMove:playersTurn];
+    RPSMove *compTurn = [[RPSMove alloc] init];
+
+    self.game = [[RPSGame alloc] initWithFirstTurn:playerTurn secondTurn:compTurn];
+}
+
+- (Move)generateMove {
+    return Rock;
+}
+
+- (BOOL)defeats:(RPSMove *)move {
+    return false;
+}
+@end
